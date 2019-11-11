3b49df75c566f0907361fa32dbe74b6b00cbf53e
Brunno Marchetti <brunno.marchetti@ibm.com>
2019-11-11 13:54:51 -0300

    data binding on elements

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,16 +8,22 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     // tslint:disable-next-line: max-line-length
-    new Recipe('Delicia', 'Teste Recipe, show!', 'https://asset.slimmingworld.co.uk/content/media/11742/chunky-spiced-chicken-burger_sw_recipe.webp?v1=JGXiore20qg9NNIj0tmc3ZeLYCPaZrsHG52q33-ZTZNzlAUyZVA3xkf8t0JL4LNZOQkDIPWYgWQCL2Cpkzpu8kFN2klwaUiIQQTq7G39dr7NQHzPyAq_hBpSyUmHsknwshcwLFi5yiTn2Pi05SU58A&width=278&height=278'),
+    new Recipe('Delicia Maior', 'Teste Recipe, show! Do tipo que é louco real e bomzão, de lamber os beiços', 'https://asset.slimmingworld.co.uk/content/media/11742/chunky-spiced-chicken-burger_sw_recipe.webp?v1=JGXiore20qg9NNIj0tmc3ZeLYCPaZrsHG52q33-ZTZNzlAUyZVA3xkf8t0JL4LNZOQkDIPWYgWQCL2Cpkzpu8kFN2klwaUiIQQTq7G39dr7NQHzPyAq_hBpSyUmHsknwshcwLFi5yiTn2Pi05SU58A&width=278&height=278'),
     // tslint:disable-next-line: max-line-length
-    new Recipe('Delicia', 'Teste Recipe, show!', 'https://asset.slimmingworld.co.uk/content/media/11742/chunky-spiced-chicken-burger_sw_recipe.webp?v1=JGXiore20qg9NNIj0tmc3ZeLYCPaZrsHG52q33-ZTZNzlAUyZVA3xkf8t0JL4LNZOQkDIPWYgWQCL2Cpkzpu8kFN2klwaUiIQQTq7G39dr7NQHzPyAq_hBpSyUmHsknwshcwLFi5yiTn2Pi05SU58A&width=278&height=278'),
+    new Recipe('Delicia', 'Teste Recipe, show! Bem boa essa também.', 'https://asset.slimmingworld.co.uk/content/media/11742/chunky-spiced-chicken-burger_sw_recipe.webp?v1=JGXiore20qg9NNIj0tmc3ZeLYCPaZrsHG52q33-ZTZNzlAUyZVA3xkf8t0JL4LNZOQkDIPWYgWQCL2Cpkzpu8kFN2klwaUiIQQTq7G39dr7NQHzPyAq_hBpSyUmHsknwshcwLFi5yiTn2Pi05SU58A&width=278&height=278'),
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
